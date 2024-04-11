6ee399d31a9a8e11b9b5d8b974f563aee68d88b0
Rafael-Mont-Medeiros <rafak1212@gmail.com>
2024-04-10 21:45:51 -0300

    TO-DO LIST HTML/CSS completada, JS em processo...

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-function add(){
-    let atividade = document.querySelector('#atividade').value
-    let tarefas = document.querySelector('#listaTarefas')
-    if(atividade == 0){
-        alert('Adicione alguma atividade no campo')
-    }else {
-      let paragrafo = document.createElement('p')
-      paragrafo.innerText = atividade
-      tarefas.appendChild(paragrafo)
-    }
 
+function addTarefa(){
+  const tarefa = document.querySelector('.input-task')
+  const listaCompleta = document.querySelector('.list-task')
+  let myList = []
+
+  myList.push(tarefa.value)
 }
+
+function mostraTarefas(){
+  let novaLi = ''
+  
+  myList.forEach(tarefa => {
+    novaLi = novaLi + `
+    <li class="task">
+      <img src="./img/checked.png" alt="">
+      <p>${tarefa}</p>
+      <img src="./img/trash.png" alt="">
+    </li>
+    `
+    
+  });
+  listaCompleta.innerHTML = novaLi
+}
+    
