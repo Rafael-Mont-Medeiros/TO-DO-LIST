3175e6cdde8129d2ff661e15fc962a6e1f0732ae
Rafael-Mont-Medeiros <rafak1212@gmail.com>
2024-04-10 21:45:51 -0300

    TO-DO LIST HTML/CSS completada, JS em processo...

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-function add(){
-    let atividade = document.querySelector('#atividade').value
-    let tarefas = document.querySelector('#listaTarefas')
-    if(atividade == 0){
-        alert('Adicione alguma atividade no campo')
-    }else {
-      let paragrafo = document.createElement('p')
-      paragrafo.innerText = atividade
-      tarefas.appendChild(paragrafo)
-    }
+const button = document.querySelector('.input-add-task')
+const input = document.querySelector('.input-task')
+const listaCompleta = document.querySelector('.list-task')
+let minhaListaDeItens = []
 
+function adicionarNovaTarefa() {
+    minhaListaDeItens.push(input.value)
+    input.value = ''
+
+    mostrarTarefas()
 }
+
+function mostrarTarefas() {
+    let novaLi = ''
+    minhaListaDeItens.forEach(tarefa => {
+        novaLi = `
+            <li class="task">
+                <img src="./img/checked.png" alt="">
+                <p>${tarefa}</p>
+                <img src="./img/trash.png" alt="">
+            </li>
+       `
+    })
+    listaCompleta.innerHTML = novaLi
+}
+
+button.addEventListener('click', adicionarNovaTarefa)
